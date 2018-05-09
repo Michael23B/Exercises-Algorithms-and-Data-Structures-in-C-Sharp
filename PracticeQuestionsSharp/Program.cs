0d4802de84dc4ed008efe317deb010bf22798596
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-05-09 15:41:24 +1000

    Fib

--- PracticeQuestionsSharp/Program.cs
+++ PracticeQuestionsSharp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Resources;
 using System.Text;
 
 namespace PracticeQuestionsSharp
@@ -197,6 +198,31 @@ namespace PracticeQuestionsSharp
         }
     }
 
+    class Fib
+    {
+        //Recursive
+        public static int Fibonacci1(int n)
+        {
+            if (n < 2) return n;
+            return Fibonacci1(n - 1) + Fibonacci1(n - 2);
+        }
+
+        //Memoized
+        public static int Fibonacci2(int n)
+        {
+            int[] fib = new int[n + 1];
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for (int i = 2; i <= n; ++i)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+
+            return fib[n];
+        }
+    }
+
     class Program
     {
         public static void Main()
@@ -233,7 +259,14 @@ namespace PracticeQuestionsSharp
             */
 
             //Palindrome permutation
+            /*
             Profiler.ProfileAndExecute(() => Palindrome.IsPalindromePermutation("acdbdacbs"));
+            */
+
+            //Fib
+
+            Profiler.ProfileAndExecute(() => Fib.Fibonacci1(25));
+            Profiler.ProfileAndExecute(() => Fib.Fibonacci2(75));
         }
     }
 }
