ec7c03af75b4b68cf9f505a1cbc502069b17c0eb
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-06-05 10:46:44 +1000

    Conversion count required to convert one integer to another integer by flipping bits
    :eyes:

---
+++ PracticeQuestionsSharp/Exercises/Bits/ConvertIntToInt.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticeQuestionsSharp.Exercises.Bits
+{
+    //Write a function to determine the number of bits you would need to flip to convert int A to int B
+    public static class ConvertIntToInt
+    {
+        public static int CountFlipsToConvertTo(this int a, int b)
+        {
+            int difference = a ^ b;
+            int result = 0;
+
+            //Count the ones in difference to see how many changes are needed
+            while (difference > 0)
+            {
+                if ((difference & 1) == 1) result++;
+                difference >>= 1;
+            }
+
+            return result;
+        }
+    }
+}
