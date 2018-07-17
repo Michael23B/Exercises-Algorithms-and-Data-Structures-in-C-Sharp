7d6e7e400e307ee31f29643493c84383dafdbb85
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-07-17 11:43:05 +1000

    Negitive number support for multiply method

--- PracticeQuestionsSharp/Exercises/Numbers/MultiplyWithBitShifting.cs
+++ PracticeQuestionsSharp/Exercises/Numbers/MultiplyWithBitShifting.cs
@@ -1,22 +1,43 @@
-﻿namespace PracticeQuestionsSharp.Exercises.Numbers
+﻿using System;
+
+namespace PracticeQuestionsSharp.Exercises.Numbers
 {
     //Perform multiplication using only bit shift operations, addition and subtraction
     public static class MultiplyWithBitShifting
     {
-        //TODO: currently doesn't work with negative numbers
         public static int Multiply(int a, int b)
         {
             if (a == 0 || b == 0) return 0;
 
-            int larger = a;
-            int smaller = b;
+            int larger;
+            int smaller;
+            bool positive = true;
             int sum = 0;
             int place = 0;
+
+            if (a < 0 && b < 0)
+            {
+                //Result is positive, get absolute values to work with
+                a = Math.Abs(a);
+                b = Math.Abs(b);
+            }
+            else if (a < 0 || b < 0)
+            {
+                //If one side is negative, the result is negative
+                positive = false;
+                a = Math.Abs(a);
+                b = Math.Abs(b);
+            }
 
             if (a < b)
             {
                 larger = b;
                 smaller = a;
+            }
+            else
+            {
+                larger = a;
+                smaller = b;
             }
 
             while (smaller != 0)
@@ -29,7 +50,7 @@
                 place++;
             }
 
-            return sum;
+            return positive ? sum: -sum;
         }
     }
 }
--- PracticeQuestionsSharp/Program.cs
+++ PracticeQuestionsSharp/Program.cs
@@ -437,6 +437,11 @@ namespace PracticeQuestionsSharp
             Console.WriteLine(MultiplyWithBitShifting.Multiply(100, 10));
             Console.WriteLine(MultiplyWithBitShifting.Multiply(23, 1));
             Console.WriteLine(MultiplyWithBitShifting.Multiply(15, 3));
+            Console.WriteLine(MultiplyWithBitShifting.Multiply(5, -6));
+            Console.WriteLine(MultiplyWithBitShifting.Multiply(-6, -5));
+            Console.WriteLine(MultiplyWithBitShifting.Multiply(-100, 10));
+            Console.WriteLine(MultiplyWithBitShifting.Multiply(-23, 1));
+            Console.WriteLine(MultiplyWithBitShifting.Multiply(-15, -3));
         }
     }
 }
